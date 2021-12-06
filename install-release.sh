6bcb42f3bb59daeda5e97d7f0c2c85a4406cc989
doudouhn6 <doudouhn6@gmail.com>
2021-12-06 14:54:18 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -272,7 +272,7 @@ get_version() {
 
 download_v2ray() {
   #DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/$RELEASE_VERSION/v2ray-linux-$MACHINE.zip"
-  DOWNLOAD_LINK="https://tiffme.eu/packages/centos/8/x86_64/v2ray-linux-64.zip"
+  DOWNLOAD_LINK="https://tinovn.eu/packages/centos/8/x86_64/v2ray-linux-64.zip"
   echo "Downloading V2Ray archive: $DOWNLOAD_LINK"
   if ! curl -x "${PROXY}" -R -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK"; then
     echo 'error: Download failed! Please check your network or try again.'
